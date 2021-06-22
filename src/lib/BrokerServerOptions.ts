2e8ad40d6c6305edf40904978b83409cd26acbc6
Luca Scaringella <lucalstyle@gmail.com>
2021-06-22 14:54:51 +0200

    Define BrokerServerOptions.

---
+++ src/lib/BrokerServerOptions.ts
@@ -0,0 +1,13 @@
+/*
+Author: Luca Scaringella
+GitHub: LucaCode
+Copyright(c) Luca Scaringella
+ */
+
+import {LogLevel} from "./Logger";
+
+export default interface BrokerServerOptions {
+    join?: string | null,
+    logLevel?: LogLevel,
+    port?: number,
+}
