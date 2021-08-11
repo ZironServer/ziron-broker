9c020b88cf0457b893fa0c2b6e0e7d8520477e3a
Luca Scaringella <lucalstyle@gmail.com>
2021-08-11 21:23:21 +0200

    Add more environment variables in the run file.

--- src/run.ts
+++ src/run.ts
@@ -6,6 +6,7 @@ Copyright(c) Luca Scaringella
 
 import { BrokerServer } from "./lib/BrokerServer";
 import { secrets } from "docker-secret";
+import { LogLevel } from "./lib/Logger";
 
 const variables = Object.assign({}, process.env, secrets);
 
@@ -16,4 +17,7 @@ new BrokerServer({
     variables.JOIN != null
       ? variables.JOIN
       : `${variables.SECRET || ""}@${variables.STATE || ""}`,
+  port: parseInt(variables.PORT) || 8888,
+  path: variables.PATH || "/",
+  logLevel: parseInt(variables.LOG_LEVEL) || LogLevel.Everything,
 }).joinAndListen();
