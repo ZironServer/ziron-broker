9d5e64d87e2e5289e654ebd116af06b650560f21
Luca Scaringella <32013416+LucaCode@users.noreply.github.com>
2022-04-20 21:50:27 +0200

    Export BrokerServerOptions.

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,6 @@ Copyright(c) Ing. Luca Gian Scaringella
 
 import { BrokerServer } from "./lib/BrokerServer";
 import { LogLevel } from "./lib/Logger";
+import BrokerServerOptions from "./lib/BrokerServerOptions";
 
-export { BrokerServer, LogLevel };
+export { BrokerServer, BrokerServerOptions, LogLevel };
